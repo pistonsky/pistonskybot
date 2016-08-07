5c7e3da12b0e745ffbff07562e3490253c334f56
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2016-08-08 00:42:51 +0500

    Set logging level to DEBUG

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import logging
 
 
-logging.basicConfig(format='%(message)s', level=logging.INFO)
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
 try:
     from local_settings import *
