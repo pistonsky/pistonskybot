0b8db347fd375de152fac5392c813e4444f245db
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2016-08-08 01:56:12 +0500

    full python2 compat

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ class Bot:
         for user_id in self.users:
             for (sender_id, voice_id, ) in new_messages:
                 if sender_id != user_id:
-                    self.http_client.fetch("{}/sendVoice".format(self.BASE_URL),
+                    self.http_client.fetch("{url}/sendVoice".format(url=self.BASE_URL),
                         method='POST', body=fast_urlencode({
                                 'chat_id': user_id,
                                 'voice': voice_id
@@ -33,7 +33,7 @@ class Bot:
 
     def _run_once(self):
         new_messages = []
-        response = self.http_client.fetch("{}/getUpdates".format(self.BASE_URL),
+        response = self.http_client.fetch("{url}/getUpdates".format(url=self.BASE_URL),
             method='POST', body=fast_urlencode({
                     'offset': self.last_update_id+1
                 }))
@@ -45,13 +45,13 @@ class Bot:
                 message = update['message']
                 sender_id = message['from']['id']
                 if sender_id not in self.users:
-                    logging.info("NEW USER: {} ({} {})".format(sender_id, message['from']['first_name'], message['from']['last_name']))
+                    logging.info("NEW USER: {id} ({first} {last})".format(id=sender_id, first=message['from']['first_name'], last=message['from']['last_name']))
                     self.users.append(sender_id) # new user
                 if 'voice' in message: # we only care about voice messages
                     voice_id = message['voice']['file_id']
                     new_messages.append((sender_id, voice_id, ))
             if len(new_messages) > 0:
-                logging.info("Broadcasting {} new audio messages...".format(len(new_messages)))
+                logging.info("Broadcasting {N} new audio messages...".format(N=len(new_messages)))
                 self.broadcast_new_messages(new_messages)
         else:
             logging.error("Telegram says that it's not okay...")
