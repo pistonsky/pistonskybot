a2a2a86282c0cc4f9a7c821d3bfac08b77fa063d
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2016-08-07 23:36:54 +0500

    Now serving HTTPS.

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
+import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+
+from settings import *
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+application = tornado.web.Application([
+    (r'/', MainHandler),
+])
+
 if __name__ == "__main__":
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-    application.listen(8888)
+    http_server = tornado.httpserver.HTTPServer(application, ssl_options={
+        "certfile": PEMFILE,
+        "keyfile": KEYFILE
+    })
+    http_server.listen(443)
     tornado.ioloop.IOLoop.current().start()
